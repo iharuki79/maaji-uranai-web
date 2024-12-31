d45ba4d94e6a7a1313ac83c00d73e27d49b160a8
iharuki79 <50833220+iharuki79@users.noreply.github.com>
2024-12-31 23:22:17 +0900

    ⛩おみくじ画像の保存機能実装

--- pages/omikuji/index.tsx
+++ pages/omikuji/index.tsx
@@ -1,7 +1,8 @@
 import type React from "react";
 import {useState} from "react";
-import styled from "./omikuji.module.scss";
+import html2canvas from "html2canvas";
 import { getTweetUrlNoLink } from "../../utils/getTweetUrl";
+import styled from "./omikuji.module.scss";
 
 const omikujiList = ["大吉🤩", "吉😁", "中吉😊", "小吉😀", "末吉🙂", "凶😨", "大凶😱"];
 
@@ -38,6 +39,25 @@ const IndexPage = () => {
     setTweetUrl(getTweetUrlNoLink(`⛩まぁじ神社⛩\n\n${tmpTitle}は…\n\n${tmpResult}！\n`, "https://uranai.hals.one/omikuji"));
   }
 
+  const onClickSave = () => {
+    const omikujiArea = document.querySelector(`.${styled.omikujiArea}`) as HTMLElement | null;
+    if ( omikujiArea ) {
+      if ( omikujiResult.length > 0 ) {
+        html2canvas(omikujiArea).then(canvas => {
+            const link = document.createElement('a');
+            link.download = 'omikuji.png';
+            link.href = canvas.toDataURL();
+            link.click();
+        });
+      } else {
+        alert("先におみくじを引いてね");
+      }
+    } else {
+      console.error("omikujiArea element not found");
+    }
+  };
+
+
   return (
     <div className={styled.all}>
       <h1 className={styled.heading}>
@@ -47,25 +67,44 @@ const IndexPage = () => {
       </h1>
       <h2 className={styled.heading2}>おみくじ</h2>
       <div className={styled.inputArea}>
-          <input
-            className={styled.todoInputField}
-            placeholder={"名前を入力"}
-            type={"text"}
-            value={userName}
-            onChange={onChangeUserName}
-          />
-          <img className={styled.drawOmikujiButton} src="/omikuji_box.png" alt="おみくじを引く" onClick={onClickDrawOmikuji} onKeyUp={onClickDrawOmikuji}/>
+        <p>おみくじ箱をクリック</p>
+        <p>名前を入力せずに何度も引くこともできます</p>
+        <input
+          className={styled.todoInputField}
+          placeholder={"名前を入力"}
+          type={"text"}
+          value={userName}
+          onChange={onChangeUserName}
+        />
+        <img className={styled.drawOmikujiButton} src="/omikuji_box.png" alt="おみくじを引く" onClick={onClickDrawOmikuji} onKeyUp={onClickDrawOmikuji}/>
       </div>
-      <div className={styled.omikujiArea}>
-        <h2 className={styled.heading2}>{resultTitle}</h2>
-        <div className={styled.omikujiResult}>{omikujiResult}</div>
+      <div className={styled.omikujiArea} style={{display: omikujiResult.length > 0 ? "block" : "none"}}>
+        <div className={styled.omikujiCard}>
+          <div className={styled.omikujiInner}>
+            <hr />
+            <h1 className={styled.heading}>
+              <span className={styled.emoji}>⛩</span>
+              <span>まぁじ神社</span>
+              <span className={styled.emoji}>⛩</span>
+            </h1>
+            <h2 className={styled.heading2}>おみくじ</h2>
+            <hr />
+            <h2 className={styled.heading2}>{resultTitle}</h2>
+            <div className={styled.omikujiResult}>{omikujiResult}</div>
+            <hr />
+          </div>
+        </div>
+      </div>
+      <div className={styled.shareArea}>
         <a
+          className={`${styled.shareButton} ${styled.twitter}`}
           href={tweetUrl}
           target="_blank"
           rel="noopener noreferrer"
         >
           ツイートする
         </a>
+        <button className={`${styled.shareButton}`} type="button" onClick={onClickSave} onKeyDown={onClickSave}>画像で保存する(png)</button>
       </div>
     </div>
   );
